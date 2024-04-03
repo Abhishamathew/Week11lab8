56d762c8ab57aa584ad09e41c5e400b7adf3c890
Abhishamathew <abhisha.mathew2k17@gmail.com>
2024-04-03 18:51:30 -0400

    Commit

---
+++ Week11.Threads/Week11.Threads/ThreadingForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Week11.Threads
+{
+    partial class ThreadingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbertxt = new System.Windows.Forms.TextBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // numbertxt
+            // 
+            this.numbertxt.Location = new System.Drawing.Point(83, 45);
+            this.numbertxt.Name = "numbertxt";
+            this.numbertxt.Size = new System.Drawing.Size(118, 22);
+            this.numbertxt.TabIndex = 0;
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Location = new System.Drawing.Point(228, 45);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(92, 23);
+            this.generateBtn.TabIndex = 1;
+            this.generateBtn.Text = "Generate";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(83, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 228);
+            this.listBox1.TabIndex = 2;
+            // 
+            // ThreadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.numbertxt);
+            this.Name = "ThreadingForm";
+            this.Text = "Multi-Threading Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numbertxt;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Week11.Threads/Week11.Threads/ThreadingForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week11.Threads
+{
+    public partial class ThreadingForm : Form
+    {
+        public ThreadingForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void generateBtn_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(numbertxt.Text, out int n))
+            {
+                Task task1 = Task.Run(() => Fibonacci(n));
+
+                listBox1.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number");
+            }
+
+
+            
+        }
+
+        private async void Fibonacci(int n)
+        {
+            
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                listBox1.BeginInvoke(new Action(() => listBox1.Items.Add(c)));
+                Thread.Sleep(100);
+            }
+
+        }
+
+
+    }
+}
